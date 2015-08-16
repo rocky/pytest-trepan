9dc4050549e2ab80e9f98f3b474425e70245096e
rocky <rocky@gnu.org>
2015-08-16 09:28:42 -0400

    Document a little better and try RsT

--- README.rst
+++ README.rst
@@ -5,6 +5,25 @@ Abstract
 A pytest plugin for running the `trepan debugger <https://pypi.python.org/pypi?:action=display&name=trepan>`_
 
 
+Using
+=====
+
+After installing, to set a breakpoint to enter the trepan debugger:
+
+    import pytest
+    def test_function():
+        ...
+         pytest.trepan()    # invoke
+	 x = 1
+	 ...
+
+
+To have the debugger entered on error, use the `--trepan` option:
+
+    $ py.test --trepan ...
+
+
+
 Project Details
 ===============
 
--- pytest_trepan/__init__.py
+++ pytest_trepan/__init__.py
@@ -1,6 +1,22 @@
 '''`pytest-trepan` is a pytest_ plugin running the `trepan debugger
 <https://pypi.python.org/pypi?:action=display&name=trepan>`_.
+
+After installing, to set a breakpoint to enter the trepan debugger:
+
+    import pytest
+    def test_function():
+        ...
+         pytest.trepan()    # invoke
+	 x = 1
+	 ...
+
+To have the debugger entered on error, use the `--trepan` option:
+
+    $ py.test --trepan ...
+
 '''
+
+__docformat__ = 'restructuredtext'
 
 version = '1.0.0'
 __version__ = version
